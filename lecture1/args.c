20f44327e04551e26ffa9b088da51f5cedf9e3c3
ben reed <ben.reed@sjsu.edu>
2022-08-22 12:06:47 -0700

    lecture 1 initial checkin

---
+++ lecture1/args.c
@@ -0,0 +1,9 @@
+//
+// Created by bcr33d on 8/22/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("got %d arguments\n", argc);
+}
---
+++ lecture1/numbers.c
@@ -0,0 +1,8 @@
+//
+// Created by bcr33d on 8/22/22.
+//
+#included <stdio.h>
+
+int main(int argc, char **argv) {
+    char *this_is_one = "1";
+}
